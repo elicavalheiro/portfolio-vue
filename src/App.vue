0edf4e9658b1361312dfcab4a306d8e8502ead80
elicavalheiro <elimara.ac@gmail.com>
2020-09-20 02:00:21 -0300

    :heavy_plus_sign: added sass config

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,35 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <Home />
+  </div>
 </template>
 
-<style>
+<script>
+import Home from "@/views/Home";
+
+export default {
+  name: "app",
+  components: {
+    Home,
+  },
+};
+</script>
+
+<style lang="scss">
+$bg-color: #1f1f1f;
+$text-color: #f1f1f1;
+$first-font-style: "Ubuntu", sans-serif;
+$second-font-style: "Alegreya Sans SC", sans-serif;
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background: $bg-color;
+  color: $text-color;
+  font-family: $first-font-style;
+}
 </style>
